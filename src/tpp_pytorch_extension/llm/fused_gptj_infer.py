c75b395fab75c89f313977932c8a0a662bf17664
Dhiraj D Kalamkar <dhiraj.d.kalamkar@intel.com>
2023-08-07 22:37:22 -0700

    Optimize for lm_head gemm

--- src/tpp_pytorch_extension/llm/fused_gptj_infer.py
+++ src/tpp_pytorch_extension/llm/fused_gptj_infer.py
@@ -216,20 +216,82 @@ def GPTJForCausalLM_forward_patched(
     output_hidden_states: Optional[bool] = None,
     return_dict: Optional[bool] = None,
 ) -> Union[Tuple, CausalLMOutputWithPast]:
-    return GPTJForCausalLM_forward(
-            self,
-            input_ids=input_ids,
-            past_key_values=past_key_values,
-            attention_mask=attention_mask,
-            position_ids=position_ids,
-            token_type_ids=token_type_ids,
-            head_mask=head_mask,
-            inputs_embeds=inputs_embeds,
-            labels=labels,
-            use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
+    r"""
+    labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+        Labels for language modeling. Note that the labels **are shifted** inside the model, i.e. you can set
+        `labels = input_ids` Indices are selected in `[-100, 0, ..., config.vocab_size]` All labels set to `-100`
+        are ignored (masked), the loss is only computed for labels in `[0, ..., config.vocab_size]`
+    """
+    return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+    transformer_outputs = self.transformer(
+        input_ids,
+        past_key_values=past_key_values,
+        attention_mask=attention_mask,
+        token_type_ids=token_type_ids,
+        position_ids=position_ids,
+        head_mask=head_mask,
+        inputs_embeds=inputs_embeds,
+        use_cache=use_cache,
+        output_attentions=output_attentions,
+        output_hidden_states=output_hidden_states,
+        return_dict=return_dict,
     )
+    hidden_states = transformer_outputs[0]
+
+    # Set device for model parallelism
+    if self.model_parallel:
+        torch.cuda.set_device(self.transformer.first_device)
+        hidden_states = hidden_states.to(self.lm_head.weight.device)
+
+    # make sure sampling in fp16 works correctly and
+    # compute loss in fp32 to match with mesh-tf version
+    # https://github.com/EleutherAI/gpt-neo/blob/89ce74164da2fb16179106f54e2269b5da8db333/models/gpt2/gpt2.py#L179
+
+    # We only need logits for last token doing text generation
+    if labels is None:
+        hidden_states = hidden_states[:, -1:, :]
+
+    lm_logits = self.lm_head(hidden_states).to(torch.float32)
+
+    loss = None
+    if labels is not None:
+        # move labels to correct device to enable model parallelism
+        labels = labels.to(lm_logits.device)
+        # Shift so that tokens < n predict n
+        shift_logits = lm_logits[..., :-1, :].contiguous()
+        shift_labels = labels[..., 1:].contiguous()
+        # Flatten the tokens
+        loss_fct = CrossEntropyLoss()
+        loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+
+        loss = loss.to(hidden_states.dtype)
+
+    if not return_dict:
+        output = (lm_logits,) + transformer_outputs[1:]
+        return ((loss,) + output) if loss is not None else output
+
+    return CausalLMOutputWithPast(
+        loss=loss,
+        logits=lm_logits,
+        past_key_values=transformer_outputs.past_key_values,
+        hidden_states=transformer_outputs.hidden_states,
+        attentions=transformer_outputs.attentions,
+    )
+    # return GPTJForCausalLM_forward(
+    #         self,
+    #         input_ids=input_ids,
+    #         past_key_values=past_key_values,
+    #         attention_mask=attention_mask,
+    #         position_ids=position_ids,
+    #         token_type_ids=token_type_ids,
+    #         head_mask=head_mask,
+    #         inputs_embeds=inputs_embeds,
+    #         labels=labels,
+    #         use_cache=use_cache,
+    #         output_attentions=output_attentions,
+    #         output_hidden_states=output_hidden_states,
+    #         return_dict=return_dict,
+    # )
 
 transformers.models.gptj.modeling_gptj.GPTJForCausalLM.forward = GPTJForCausalLM_forward_patched
