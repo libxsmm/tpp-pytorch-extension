a984788324f6d54261c201683720475b536d246f
Dhiraj D Kalamkar <dhiraj.d.kalamkar@intel.com>
2023-08-13 22:33:46 -0700

    Fix build error

--- setup.py
+++ setup.py
@@ -133,7 +133,7 @@ sources += glob.glob("src/csrc/gnn/gat/*.cpp")
 sources += glob.glob("src/csrc/dlrm/*.cpp")
 
 
-extra_compile_args = ["-fopenmp", "-g"]  # , "-O0"]
+extra_compile_args = ["-fopenmp", "-g", "-DLIBXSMM_DEFAULT_CONFIG"]  # , "-O0"]
 if platform.processor() != "aarch64":
     extra_compile_args.append("-march=native")
 
