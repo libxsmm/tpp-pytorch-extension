bc49fb6393ef544fc26cc4121306789a17dd0106
Dhiraj D Kalamkar <dhiraj.d.kalamkar@intel.com>
2023-08-17 04:41:59 -0700

    Enable SK padding for 1st token

--- src/csrc/llm/fused_llm_infer.cpp
+++ src/csrc/llm/fused_llm_infer.cpp
@@ -1449,7 +1449,7 @@ inline at::Tensor attn(
 
   int vl_in_vnni = 1; //(Sk % 2 == 0 ? 1 : 0);
   const long VBS = (vl_in_vnni ? get_vnni_block_size<T>() : 1);
-  const long SK_ALIGN = (VBS == 1 ? 1 : VBS);
+  const long SK_ALIGN = (VBS == 1 ? 1 : SK_BLOCK_SIZE);
   long Sk_pad = (Sk + SK_ALIGN - 1) & ~(SK_ALIGN - 1);
   const long Skb = (!inline_trans ? 512 : SK_BLOCK_SIZE);
   long krem = Sk % Skb;
