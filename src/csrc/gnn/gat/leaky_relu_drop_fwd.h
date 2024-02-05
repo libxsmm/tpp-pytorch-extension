07f72169a2ff1ec04cd801ba4144505becd0fec6
Sasikanth Avancha <sasikanth.avancha@intel.com>
2024-02-05 09:04:12 -0500

    LeakyReLUDropout C++ code

--- src/csrc/gnn/gat/leaky_relu_drop_fwd.h
+++ src/csrc/gnn/gat/leaky_relu_drop_fwd.h
@@ -26,30 +26,44 @@ auto lrelu_mask = t_lrelu_mask.data_ptr<short>();
 auto dp_mask = t_dp_mask.data_ptr<short>();
 const int BS = 256; // Define the block size
 
-auto leaky_relu_fwd_tpp = SCOPEIT(LeakyReLUFwdTPP<T>(BS, alpha), ACT);
-auto dropout_fwd_tpp = SCOPEIT(DropOutFwdTPP<T>(BS, p), DROPOUT);
-{
-  RECORD_SCOPE(gao_lrelu_drop, {t_in});
+if(training) {
+  auto leaky_relu_fwd_tpp = SCOPEIT(LeakyReLUFwdTPP<T>(BS, alpha), ACT);
+  auto dropout_fwd_tpp = SCOPEIT(DropOutFwdTPP<T>(BS, p), DROPOUT);
   {
-    RECORD_FUNCTION("parallel_for", std::vector<c10::IValue>());
-    long n;
-    /* The omp parallel loop will run for all the blocks of N except the last
-       block by using lastprivate() the ALIGNDOWN() it takes care of the blocks
-       for the 1D tensor*/
+    RECORD_SCOPE(gao_lrelu_drop, {t_in});
+    {
+      RECORD_FUNCTION("parallel_for", std::vector<c10::IValue>());
+      long n;
+      /* The omp parallel loop will run for all the blocks of N except the last
+	 block by using lastprivate() the ALIGNDOWN() it takes care of the blocks
+	 for the 1D tensor*/
 #pragma omp parallel for lastprivate(n)
-    for (n = 0; n < ALIGNDOWN(N, BS); n += BS) {
-      leaky_relu_fwd_tpp(&in[n], &out[n], &lrelu_mask[n / 16]);
-      dropout_fwd_tpp(
-          &out[n], (void*)get_rng_state(), &out[n], &dp_mask[n / 16]);
+      for (n = 0; n < ALIGNDOWN(N, BS); n += BS) {
+	leaky_relu_fwd_tpp(&in[n], &out[n], &lrelu_mask[n / 16]);
+	dropout_fwd_tpp(
+	    &out[n], (void*)get_rng_state(), &out[n], &dp_mask[n / 16]);
+      }
+      // The reminder part is handled here
+      if (n < N) {
+	auto leaky_relu_fwd_tpp = SCOPEIT(LeakyReLUFwdTPP<T>(N - n, alpha), ACT);
+	auto dropout_fwd_tpp = SCOPEIT(DropOutFwdTPP<T>(N - n, p), DROPOUT);
+	leaky_relu_fwd_tpp(&in[n], &out[n], &lrelu_mask[n / 16]);
+	dropout_fwd_tpp(
+	    &out[n], (void*)get_rng_state(), &out[n], &dp_mask[n / 16]);
+      }
     }
-    // The reminder part is handled here
-    if (n < N) {
-      auto leaky_relu_fwd_tpp = SCOPEIT(LeakyReLUFwdTPP<T>(N - n, alpha), ACT);
-      auto dropout_fwd_tpp = SCOPEIT(DropOutFwdTPP<T>(N - n, p), DROPOUT);
-      leaky_relu_fwd_tpp(&in[n], &out[n], &lrelu_mask[n / 16]);
-      dropout_fwd_tpp(
-          &out[n], (void*)get_rng_state(), &out[n], &dp_mask[n / 16]);
-    }
+  }
+}
+else {
+  auto leaky_relu_fwd_tpp = SCOPEIT(LeakyReLUFwdTPP<T>(BS, alpha), ACT);
+  long n;
+#pragma omp parallel for lastprivate(n)
+  for (n = 0; n < ALIGNDOWN(N, BS); n += BS) {
+    leaky_relu_fwd_tpp(&in[n], &out[n], &lrelu_mask[n / 16]);
+  }
+  if (n < N) {
+    auto leaky_relu_fwd_tpp = SCOPEIT(LeakyReLUFwdTPP<T>(N - n, alpha), ACT);
+    leaky_relu_fwd_tpp(&in[n], &out[n], &lrelu_mask[n / 16]);
   }
 }
 
